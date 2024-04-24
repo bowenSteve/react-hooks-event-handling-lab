3f78166e169859ad86df3b13f1924214edc8cf49
Stephen Bowen <bowensteven17@gmail.com>
2024-04-24 21:11:34 +0300

    Complete event handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function change(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input onChange={change}  type="password"/>
+        </div>
     )
 }
 
